07a2a594660ab3af3f5d1369981ec3d9472a2fd4
PedroAF <128552048+vscvolley@users.noreply.github.com>
2024-05-16 22:25:42 +0100

    modified:   Gamificacao-App/package-lock.json 	modified:   Gamificacao-App/package.json 	modified:   Gamificacao-App/src/App.vue 	modified:   Gamificacao-App/src/assets/base.css 	modified:   Gamificacao-App/src/assets/main.css 	new file:   Gamificacao-App/src/assets/pngwing.com.png 	modified:   Gamificacao-App/src/main.js 	modified:   Gamificacao-App/src/views/HomeView.vue

--- Gamificacao-App/src/main.js
+++ Gamificacao-App/src/main.js
@@ -4,8 +4,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import vue3GoogleLogin from 'vue3-google-login'
+const authkey = '415034066399-imnm6opf083t2dptfdvratlgp6p89tfh.apps.googleusercontent.com'
+
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
 const app = createApp(App)
 
 app.use(router)
-
+app.use(vue3GoogleLogin, { clientId: authkey })
+app.use(BootstrapVue3)
 app.mount('#app')
